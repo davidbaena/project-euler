049f15a13cdecd283b1bc193ac1980194ad0d6c3
david.baena <david.baena@bq.com>
2018-11-09 16:12:25 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Lib
+
+main :: IO ()
+--main = do
+--  putStrLn ("Multiples: " ++ show (multiples [1 .. 30]))
+--  putStrLn ("Sum of Multiples: " ++ show (euler001 [1 .. 999]))
+main = do
+  putStrLn ("Euler004: " ++ show (euler004))
